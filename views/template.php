459cadeb9c7ba7dc306841bff5f742157be3f90a
vakter23 <vakterpro@hotmail.com>
2020-12-30 17:29:02 +0100

    Add: Image

--- views/template.php
+++ views/template.php
@@ -1,17 +1,25 @@
 <!DOCTYPE html>
-<html>
+<html lang="fr">
     <head>
-
-        <meta charset="utf8" />
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
         <title><?= $t ?></title>
     </head>
-    <header>
-        <h1><a href="<?= URL ?>">Accueil</a></h1>
-    </header>
-    <nav>
-       <?= $content ?>
-    </nav>
-    <footer>
-        <p>CONTRII</p>
-    </footer>
+
+    <body class="w-100 p-3">
+        <header>
+            <h1><a href="<?= URL ?>">Accueil</a></h1>
+        </header>
+        <main class="w-75 p-3">
+            <?= $content ?>
+        </main>
+        <footer>
+            <p>CONTRII</p>
+        </footer>
+    </body>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
 </html>
--- views/viewAccueil.php
+++ views/viewAccueil.php
@@ -1,7 +1,15 @@
-<?php  $this->_t = 'Contribooks';
+<?php  $this->_t = 'Contribooks'; ?>
 
-foreach($books as $article): ?>
-<h2><?= $article->getISBN() ?></h2>
-    <p><?= $article->getId_author() ?></p>
+<ul class="nav justify-content-center mw-25">
+    <?php foreach($books as $book): ?>
+        <?php $ISBN = $book->getISBN();
+        $img_link = "$ISBN".'.jpg';
+        $filename = 'utils/media/img/'.$img_link;
+        ?>
 
-<?php endforeach; ?>
+        <li class="nav-item">
+            <a class="nav-link active" href="#="><?= $book->getTitle_book() ?></a>
+            <img class="img-thumbnail img-responsive" src="<?= $filename ?>" style="width: 150px; height: 204px">
+        </li>
+    <?php endforeach; ?>
+</ul>
