23ca5c7fbf2c8f6d6baf8ecf1b85e5dfc8b1698f
Djibril QUENUM-SANFO <d.quenumsanfo@gmail.com>
2021-01-06 14:02:04 +0100

    Improve : Accueil (Images Livres)

--- views/viewAccueil.php
+++ views/viewAccueil.php
@@ -3,18 +3,18 @@
 <div class="container">
     <h2><a>Nouveautés</a></h2>
     <ul class="nav justify-content-center mw-25">
-    <?php foreach($newBooks as $book): ?>
-        <?php $ISBN = $book->getISBN();
-        $img_link = "$ISBN".'.jpg';
-        $filename = 'utils/media/img/'.$img_link;
-        ?>
+        <?php foreach($newBooks as $book): ?>
+            <?php $ISBN = $book->getISBN();
+            $img_link = "$ISBN".'.jpg';
+            $filename = 'utils/media/img/book/'.$img_link;
+            ?>
 
-        <li class="nav-item">
-            <img class="d-inline-flex p-2" src="<?= $filename ?>" style="width: 150px; height: 204px">
-            <a class="nav-link" href="viewBook.php?ISBN=<?= $ISBN ?>"><?= $book->getTitle_book() ?></a>
-        </li>
-    <?php endforeach; ?>
-</ul>
+            <li class="nav-item">
+                <img class="d-inline-flex p-2" src="<?= $filename ?>" style="width: 150px; height: 204px">
+                <a class="nav-link" href="viewBook.php?ISBN=<?= $ISBN ?>"><?= $book->getTitle_book() ?></a>
+            </li>
+        <?php endforeach; ?>
+    </ul>
 </div>
 <div class="container">
     <h2><a>Meilleurs livres du moments</a></h2>
@@ -22,7 +22,7 @@
         <?php foreach($newBooks as $book): ?>
             <?php $ISBN = $book->getISBN();
             $img_link = "$ISBN".'.jpg';
-            $filename = 'utils/media/img/'.$img_link;
+            $filename = 'utils/media/img/book/'.$img_link;
             ?>
 
             <li class="nav-item">
