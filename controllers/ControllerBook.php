4e2b03ef42eacc906dfeb57c6da1981120a631c9
Djibril QUENUM-SANFO <d.quenumsanfo@gmail.com>
2021-01-14 21:31:27 +0100

    Commit adding Book and Review part2

---
+++ controllers/ControllerBook.php
@@ -0,0 +1,35 @@
+<?php
+require_once('core/View.php');
+
+class ControllerBook
+{
+    private $_isbn;
+    private $view;
+    private $_bookManager;
+
+    public function __construct($url)
+    {
+        //$url = 2294756290;
+
+        if(isset($url) && count( array($url) )>1)
+            throw new Exception('Page Introuvable');
+        else {
+            $this->_isbn = explode('=', $_SERVER['REQUEST_URI']);
+            $this->getBook();
+        }
+    }
+
+    private function getBook(){
+        $this->_bookManager = new BookManager;
+        $book = $this->_bookManager->getBookByISBN($this->_isbn[1]);
+        $reviews = $this->_bookManager->getReviewsByISBN($this->_isbn);
+        //var_dump($book);
+        $this->view = new View('Book');
+        $this->view->generate(array('book' => $book, 'reviews' => $reviews));
+
+    }
+
+    private function getHttp(){
+
+    }
+}
