5f7e84b4e2dd7c1704463c046ca8c9f01e29711c
vakter23 <vakterpro@hotmail.com>
2021-01-22 08:34:57 +0100

    Final commit

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 $data = $_SERVER['REQUEST_URI'];
 $urlCss = substr($data, strrpos($data, '/' )+1)."\n";
-
 define('ACTUALPAGE',$urlCss);
 define('URL',
 str_replace("index.php", "Accueil" , isset($_SERVER['HTTPS']) ? "https" : "http" . "://$_SERVER[HTTP_HOST]$_SERVER[PHP_SELF]"));
