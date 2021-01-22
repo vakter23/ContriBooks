35db8305338813d528f64e9a8adefb9a7ef17a02
Volkan AKTER <60293356+vakter23@users.noreply.github.com>
2021-01-22 07:16:49 +0100

    Delete index.php

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-    echo ("Beginner");
-    echo ("Djibril est passé par ici");
-    echo ("Djibril est repassé par ici");
-    echo ("Djibril se barre d'ici");
-    echo ("Hier on avait R ajd on a tout");
-    echo ("OUI");
-?>
